311edb1265cfd07f0d3c5cf78998b2f81ecafc11
Princeden Hom <princedenhom@gmail.com>
2026-07-08 10:14:12 -0400

    Enhance setup.sh for Docker and Isaac ROS installation
    Updated the setup script to install Docker with GPU support and clone the Isaac ROS repository.

--- setup.sh
+++ setup.sh
@@ -1,16 +1,14 @@
+#!/usr/bin/env bash
 set -euo pipefail
 
-
 echo "Setting up docker"
-# Add Docker's official GPG key:
 sudo apt update
-sudo apt install ca-certificates curl
+sudo apt install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+sudo tee /etc/apt/sources.list.d/docker.sources > /dev/null <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
 Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
@@ -20,10 +18,30 @@ Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
 sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker "$USER"
+echo "Configuring docker to use GPU"
+
+sudo apt install -y jq
+sudo jq '. + {"default-runtime": "nvidia"}' /etc/docker/daemon.json | \
+    sudo tee /etc/docker/daemon.json.tmp && \
+    sudo mv /etc/docker/daemon.json.tmp /etc/docker/daemon.json
 echo "Done setting up docker"
 
 echo "Setting up isaac ros environment"
-echo "cloning rpo"
-git clone https://github.com/NVIDIA-ISAAC-ROS/isaac_ros_common/tree/release-3.2#
+mkdir -p ~/workspaces/isaac_ros-dev/src
+cd ~/workspaces/isaac_ros-dev/src
+
+echo "cloning repo"
+git clone -b release-3.2 https://github.com/NVIDIA-ISAAC-ROS/isaac_ros_common.git
+chmod +x isaac_ros_common/scripts/run_dev.sh
+
+echo "export ISAAC_ROS_WS=${HOME}/workspaces/isaac_ros-dev" >> ~/.bashrc
+
+echo "Done. Run the following to start Isaac ROS:"
+echo "  cd ~/workspaces/isaac_ros-dev/src/isaac_ros_common && ./scripts/run_dev.sh"
+
+echo "Done, execute /isaac_ros_common/scripts/run_dev.sh to start isaac ros"
 
 
